e242774e4bd43a37c56f239abd430c1a5837f7c2
ssqyht <ssqyht@163.com>
2019-06-14 17:28:21 +0800

    Create docker-entryenv.sh

---
+++ yapi/docker-entryenv.sh
@@ -0,0 +1,22 @@
+{
+   "port": "SERVER_PORT",
+   "adminAccount": "ADMIN_ACCOUNT",
+   "db": {
+      "servername": "MONGO_URL",
+      "DATABASE": "MONGO_DATABASE",
+      "port": "MONGO_PORT",
+      "user": "MONGO_USER",
+      "pass": "MONGO_PASS"
+   },
+   "mail": {
+      "enable": true,
+      "host": "EMAIL_HOST",
+      "port": "EMAIL_PORT",
+      "from": "EMAIL_FROM",
+      "auth": {
+         "user": "EMAIL_USER",
+         "pass": "EMAIL_PASS"
+      }
+   }
+}
+
